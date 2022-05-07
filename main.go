8f073cfb5a9e18a804d607a6ccf8b5ae5b573fed
CHRIS_LEE <chrismlee87@gmail.com>
2022-05-06 21:09:12 -0700

    feat: add struct, assign values and print

--- main.go
+++ main.go
@@ -2,6 +2,11 @@ package main
 
 import "fmt"
 
+type Person struct {
+	name string
+	job  string
+}
+
 func main() {
 	i := 1
 	max := 20
@@ -10,4 +15,10 @@ func main() {
 		fmt.Println(i)
 		i += 1
 	}
+	var aperson Person
+	aperson.name = "Albert"
+	aperson.job = "Developer"
+
+	fmt.Printf("aperson.name = %s\n", aperson.name)
+	fmt.Printf("aperson.job = %s\n", aperson.job)
 }
