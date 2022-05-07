1a3a024603ede678d27925d82be0989acccb24c4
CHRIS_LEE <chrismlee87@gmail.com>
2022-05-06 22:02:57 -0700

    feat: mvp complete, onto formatting output

--- main.go
+++ main.go
@@ -1,24 +1,41 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
 
-type Person struct {
-	name string
-	job  string
+type House struct {
+	numberOfRooms int
+	city          string
+	address       string
+	price         int
 }
 
 func main() {
-	i := 1
-	max := 20
+	newHouse := []House{}
+	reader := bufio.NewReader(os.Stdin)
 
-	for i < max {
-		fmt.Println(i)
-		i += 1
+	fmt.Println("Enter number of rooms in the house:")
+	user_num_rooms, _ := reader.ReadString('\n')
+	int_user_num_rooms, _ := strconv.ParseInt(strings.TrimSpace(user_num_rooms), 10, 32)
+	fmt.Println("Enter city:")
+	user_city, _ := reader.ReadString('\n')
+	fmt.Println("Enter address:")
+	user_address, _ := reader.ReadString('\n')
+	fmt.Println("Enter price:")
+	user_price, _ := reader.ReadString('\n')
+	int_user_price, _ := strconv.ParseInt(strings.TrimSpace(user_price), 10, 32)
+
+	newHouse = append(newHouse, House{int(int_user_num_rooms), user_city, user_address, int(int_user_price)})
+
+	for _, house := range newHouse {
+		fmt.Printf("\r%v\t %s\t %s\t %v", house.numberOfRooms, house.city, house.address, house.price)
 	}
-	var aperson Person
-	aperson.name = "Albert"
-	aperson.job = "Developer"
 
-	fmt.Printf("aperson.name = %s\n", aperson.name)
-	fmt.Printf("aperson.job = %s\n", aperson.job)
+	// fmt.Println(newHouse)
+
 }
