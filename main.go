d57b29b739120b34ac947fefa2bcd5dacb494fcc
CHRIS_LEE <chrismlee87@gmail.com>
2022-05-06 22:49:48 -0700

    feat: loop program until user exits -- exit is working, but continue only prints a single input

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 )
 
 type House struct {
-	numberOfRooms int
+	numberOfRooms string
 	city          string
 	address       string
 	price         int
@@ -19,23 +19,37 @@ func main() {
 	newHouse := []House{}
 	reader := bufio.NewReader(os.Stdin)
 
-	fmt.Println("Enter number of rooms in the house:")
-	user_num_rooms, _ := reader.ReadString('\n')
-	int_user_num_rooms, _ := strconv.ParseInt(strings.TrimSpace(user_num_rooms), 10, 32)
-	fmt.Println("Enter city:")
-	user_city, _ := reader.ReadString('\n')
-	fmt.Println("Enter address:")
-	user_address, _ := reader.ReadString('\n')
-	fmt.Println("Enter price:")
-	user_price, _ := reader.ReadString('\n')
-	int_user_price, _ := strconv.ParseInt(strings.TrimSpace(user_price), 10, 32)
+	for {
+		fmt.Println("Enter number of rooms in the house:")
+		user_num_rooms, _ := reader.ReadString('\n')
+		fmt.Println("Enter city:")
+		user_city, _ := reader.ReadString('\n')
+		fmt.Println("Enter address:")
+		user_address, _ := reader.ReadString('\n')
+		fmt.Println("Enter price:")
+		user_price, _ := reader.ReadString('\n')
+		int_user_price, _ := strconv.ParseInt(strings.TrimSpace(user_price), 10, 32)
 
-	newHouse = append(newHouse, House{int(int_user_num_rooms), user_city, user_address, int(int_user_price)})
+		fmt.Println("Continue? (y/n)")
+		continue_loop, _ := reader.ReadString('\n')
 
-	for _, house := range newHouse {
-		fmt.Printf("\r%v\t %s\t %s\t %v", house.numberOfRooms, house.city, house.address, house.price)
+		if strings.TrimSpace(continue_loop) == "y" {
+			newHouse = append(newHouse, House{user_num_rooms, user_city, user_address, int(int_user_price)})
+		} else {
+			newHouse = append(newHouse, House{user_num_rooms, user_city, user_address, int(int_user_price)})
+			fmt.Println("Exiting...")
+			for _, house := range newHouse {
+				fmt.Println("\n ~~~~~~~Houses for Sale:~~~~~~~")
+				fmt.Printf("%-5s %-5s %-5s rooms %d \n", house.address, house.city, house.numberOfRooms, house.price)
+			}
+			os.Exit(0)
+		}
+
+		// newHouse = append(newHouse, House{user_num_rooms, user_city, user_address, int(int_user_price)})
+
+		for _, house := range newHouse {
+			fmt.Println("\n ~~~~~~~Houses for Sale:~~~~~~~")
+			fmt.Printf("%-5s %-5s %-5s rooms %d \n", house.address, house.city, house.numberOfRooms, house.price)
+		}
 	}
-
-	// fmt.Println(newHouse)
-
 }
